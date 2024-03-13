c49f076e16fe2b81ed2f1398b15632a05309f6fd
CHB-0r1s <borisluzin2004@gmail.com>
2024-03-13 11:47:23 +0300

    system method correct

--- lab2/nlin_eq_solve_methods.py
+++ lab2/nlin_eq_solve_methods.py
@@ -31,7 +31,10 @@ def simple_iterations_method(func, interval, epsilon):
     expr_diff_a = abs(s_e_diff.subs(x_sym, a))
     expr_diff_b = abs(s_e_diff.subs(x_sym, b))
 
-    lm_res = (-1 / max(expr_diff_a, expr_diff_b))
+    if s_e_diff.subs(x_sym, a) > 0:
+        lm_res = -1 / max(expr_diff_a, expr_diff_b)
+    else:
+        lm_res = 1 / max(expr_diff_a, expr_diff_b)
 
     phi_res = phi.subs(lm, lm_res)
 
@@ -58,7 +61,7 @@ def simple_iterations_method(func, interval, epsilon):
         x_cur = phi_res.subs(x_sym, x_prev)
         n += 1
 
-    return x_cur, func(x_cur), n
+    return float(x_cur), float(func(x_cur)), n
 
 
 def newton_method(func, interval, epsilon):
@@ -88,24 +91,56 @@ def simple_iterations_method_for_system_2(func1, func2, intervals, epsilon):
     x1_sym = sp.symbols('x_1')
     x2_sym = sp.symbols('x_2')
 
+    lm_1 = sp.symbols('lambda_1')
+    lm_2 = sp.symbols('lambda_2')
+
     x_1_lower_bound, x_1_upper_bound = intervals[0]
     x_2_lower_bound, x_2_upper_bound = intervals[1]
 
     start_expr_1 = func1(x1_sym, x2_sym)
     start_expr_2 = func2(x1_sym, x2_sym)
 
-    phi_1 = -1 * (start_expr_1 - x1_sym)
-    phi_2 = -1 * (start_expr_2 - x2_sym)
+    phi_1 = x1_sym + lm_1 * start_expr_1
+    phi_2 = x2_sym + lm_2 * start_expr_2
 
-    if not (
+    s_e_1_diff_x1 = sp.diff(start_expr_1, x1_sym)
+    s_e_1_diff_x2 = sp.diff(start_expr_1, x2_sym)
+    s_e_1_diff_G = [s_e_1_diff_x1.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_lower_bound),
+                    s_e_1_diff_x1.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_upper_bound),
+                    s_e_1_diff_x1.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_lower_bound),
+                    s_e_1_diff_x1.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound),
+                    s_e_1_diff_x2.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_lower_bound),
+                    s_e_1_diff_x2.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_upper_bound),
+                    s_e_1_diff_x2.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_lower_bound),
+                    s_e_1_diff_x2.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound)]
+    lm_phi_1 = max(s_e_1_diff_G)
+    phi_1 = phi_1.subs(lm_1, -1 / lm_phi_1)
+
+    s_e_2_diff_x1 = sp.diff(start_expr_2, x1_sym)
+    s_e_2_diff_x2 = sp.diff(start_expr_2, x2_sym)
+    s_e_2_diff_G = [
+        s_e_2_diff_x1.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_lower_bound),
+        s_e_2_diff_x1.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_upper_bound),
+        s_e_2_diff_x1.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_lower_bound),
+        s_e_2_diff_x1.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound),
+        s_e_2_diff_x2.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_lower_bound),
+        s_e_2_diff_x2.subs(x1_sym, x_1_lower_bound).subs(x2_sym, x_2_upper_bound),
+        s_e_2_diff_x2.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_lower_bound),
+        s_e_2_diff_x2.subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound)
+    ]
+    lm_phi_2 = max(s_e_2_diff_G)
+    phi_2 = phi_2.subs(lm_2, -1 / lm_phi_2)
+    # phi_2 = -1 * (3 * (x1_sym ** 3) + 2)
+
+    if (
             bool(
-                    abs(sp.diff(phi_1, x1_sym).subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound)) +
-                    abs(sp.diff(phi_1, x2_sym).subs(x2_sym, x_2_upper_bound).subs(x1_sym, x_1_upper_bound)) < 1
+                abs(sp.diff(phi_1, x1_sym).subs(x1_sym, x_1_upper_bound - epsilon).subs(x2_sym, x_2_upper_bound)) +
+                abs(sp.diff(phi_1, x2_sym).subs(x2_sym, x_2_upper_bound + epsilon).subs(x1_sym, x_1_upper_bound)) < 1
             )
             and
             bool(
-                    abs(sp.diff(phi_2, x1_sym).subs(x1_sym, x_1_upper_bound).subs(x2_sym, x_2_upper_bound)) +
-                    abs(sp.diff(phi_2, x2_sym).subs(x2_sym, x_2_upper_bound).subs(x1_sym, x_1_upper_bound)) < 1
+                abs(sp.diff(phi_2, x1_sym).subs(x1_sym, x_1_upper_bound - epsilon).subs(x2_sym, x_2_upper_bound)) +
+                abs(sp.diff(phi_2, x2_sym).subs(x2_sym, x_2_upper_bound).subs(x1_sym, x_1_upper_bound - epsilon)) < 1
             )
     ):
         print("Не сходиться! Задайте интервалы более строго.")
@@ -126,3 +161,11 @@ def simple_iterations_method_for_system_2(func1, func2, intervals, epsilon):
         n += 1
 
     return (x1_cur, x2_cur), start_expr_1.subs(x1_sym, x1_cur).subs(x2_sym, x2_cur), n
+
+
+print(simple_iterations_method_for_system_2(
+    lambda x, y: 2 * x ** 2 - y - 5,
+    lambda x, y: 3 * x ** 3 + y + 2,
+    [[0.1, 0.83], [-4, -3.5]],
+    10 ** -5
+))
